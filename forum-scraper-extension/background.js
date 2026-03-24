e17a27f9c34ddd3e70d4bb9c86f09d5243bba808
syndrome4002-leo <syndrome4002@gmail.com>
2026-03-24 17:43:38 -0400

    Done version

--- forum-scraper-extension/background.js
+++
@@ -1,4 +0,0 @@
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('ICN Forum Topics Scraper installed.');
-});
-
--- posts-scraper-extension/background.js
+++
@@ -1,4 +0,0 @@
-console.log('ICN Posts Scraper background worker running');
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('ICN Posts Scraper installed');
-});
